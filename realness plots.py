cc973f3edb1add1fdeb277dd9d0a062257880a51
YuvalSK <samoilov.y@gmail.com>
2024-09-26 20:35:48 +0300

    sorted data for visualizatio

--- realness plots.py
+++ realness plots.py
@@ -34,15 +34,46 @@ plt.ylabel('Mean realness [A.U.]')
 plt.savefig(f'Res/average over trials.png')
 
 #inidivdual-level analysis - realness scores
-plt.clf()
 df_plot = df.loc[:, subjects]
-labels = df['image'].to_list()
+df_plot['image'] = df['image']
 
-df_bars = df_caps.sort_values(by=['Total_CAPS_Score'])
-sorted_ids = [str(i) for i in df_bars['participant_id'].to_list()]
+
+df_by_caps = df_caps.sort_values(by=['Total_CAPS_Score'])
+
+#inidivdual-level analysis - CAPS scores
+fig, ax = plt.subplots(1,1)
+x_t = np.arange(1, len(subjects)+1)
+ax.bar(x = x_t,height = df_by_caps['Total_CAPS_Score'], facecolor='none', edgecolor='k')
+ax.set_xlim(0, 52)
+ax.set_ylim(-0.5, 20)
+plt.xticks(x_t, df_by_caps['participant_id'], fontsize = 6, rotation=45)
+plt.xlabel('Subjects')
+plt.ylabel('Total CAPS score')
+sns.despine()
+plt.tight_layout()
+plt.savefig(f'Res/CAPS_sorted.png', dpi=800)
+
+
+df_sorted = pd.DataFrame()
+sorted_ids = [str(i) for i in df_by_caps['participant_id'].to_list()]
+sorted_ids.append('image')
 df_sorted = df_plot[sorted_ids].copy()
 
+df_sorted['avg_realness_image'] = df_sorted.iloc[:,:-1].mean(axis=1)
 
+plt.scatter(df_sorted['Visual_sim'], df_sorted['image'], facecolor='none', edgecolor='k', s=20)
+plt.ylim(-1, 95)
+plt.xlim(0.84, 1.0)
+plt.yticks(fontsize = 4)
+plt.ylabel('Stimuli')
+plt.xlabel('Visual similarity of pairs of animals [cosine]')
+sns.despine()
+plt.tight_layout()
+plt.savefig(f'Res/images_by_CLIP_sorted.png', dpi=800)
+
+
+df_sorted['Visual_sim'] = df['Visual Similarity']
+df_sorted = df_sorted.sort_values(by=['Visual_sim'],ascending=False)
 
 #adding the average realness report
 '''
@@ -51,9 +82,13 @@ df_plot.loc[len(df_plot.index)] = avg
 labels.append("Avg. realness")
 '''
 sns.set_style('ticks')
-ax = sns.heatmap(df_sorted, 
-                 cmap='coolwarm', yticklabels=labels, xticklabels=True)
-ax.tick_params(axis='both', which='major', width = 0.2,length=3, labelsize=3, labelbottom = False, bottom=False, top = False, labeltop=True)
+
+labels = df_sorted['image'].to_list()
+ax = sns.heatmap(df_sorted.iloc[:,:-3], 
+                 cmap='coolwarm', cbar_kws={'label': 'Realness score'},
+                 yticklabels=labels, xticklabels=True)
+ax.tick_params(axis='both', which='major', width = 0.2,length=3, labelsize=3)
+#labelbottom = False, bottom=False, top = False, labeltop=True
 plt.setp(ax.xaxis.get_majorticklabels(), rotation=45)
 
 plt.ylabel('Stimuli', fontsize = 10)
@@ -61,19 +96,7 @@ plt.xlabel('Subjects', fontsize = 8)
 plt.tight_layout()
 plt.savefig(f'Res/heatmap_sorted.png', dpi=800)
 
-#inidivdual-level analysis - CAPS scores
 
-fig, ax = plt.subplots(1,1)
-ax.bar(x = np.arange(1, len(subjects)+1),height = df_bars['Total_CAPS_Score'], facecolor='none', edgecolor='k')
-ax.xaxis.set_ticks(np.arange(1, len(subjects)+1))
-ax.set_xlim(0, 52)
-ax.set_ylim(-0.5, 20)
-plt.xticks(fontsize = 9, rotation=45)
-plt.xlabel('Subjects')
-plt.ylabel('Total CAPS score')
-sns.despine()
-plt.tight_layout()
-plt.savefig(f'Res/CAPS.png', dpi=800)
 
 #individual plots
 fig = plt.figure(figsize=[12,8])
